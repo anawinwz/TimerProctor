1d86ceba39f7cf770d697b5e8a22b6ac03974369
Anawin W <anawinwz@users.noreply.github.com>
2020-10-06 03:20:13 +0700

    Add dummy login function to navigate

--- timerproctor/components/exams/IntroLogin.js
+++ timerproctor/components/exams/IntroLogin.js
@@ -1,14 +1,23 @@
-import { Card, Row, Col, Space, Button } from 'antd'
+import { useCallback } from 'react'
+import { useRouter } from 'next/router'
+import { Card, Space,Button } from 'antd'
 import { Google } from '../icons'
 
 const IntroLogin = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  const login = useCallback(() => {
+    router.replace(`/exams/${id}/authenticate`)
+  }, [router])
+
   return (
     <Card className="text-center">
       <p>โปรดเข้าสู่ระบบเพื่อดำเนินการต่อ</p>
       
       <Space direction="vertical">
-        <Button block icon={<span className="anticon"><Google /></span>}>เข้าสู่ระบบด้วย Google</Button>
-        <Button block>OpenID: Kasetsart University</Button>
+        <Button onClick={login} block icon={<span className="anticon"><Google /></span>}>เข้าสู่ระบบด้วย Google</Button>
+        <Button onClick={login} block>OpenID: Kasetsart University</Button>
       </Space>
     </Card>
   )
