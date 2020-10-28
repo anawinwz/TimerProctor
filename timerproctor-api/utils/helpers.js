4b0b6fd28f2acb18747e0b1b6311fc92ea30b54a
Anawin W <anawinwz@users.noreply.github.com>
2020-10-29 05:12:15 +0700

    Parse into JSON string before broadcast

--- timerproctor-api/utils/helpers.js
+++ timerproctor-api/utils/helpers.js
@@ -1,9 +1,9 @@
 export const jsonResponse = (status = 'ok', message = '') => ({ status, message })
 
-export const wsBroadcast = (wss, data) => {
+export const wsBroadcast = (wss, data = {}) => {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(data)
+      client.send(JSON.stringify(data))
     }
   })
 }
