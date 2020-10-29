7acfa9f2087bf51405cc773a5935d8b9c413433b
Anawin W <anawinwz@users.noreply.github.com>
2020-10-30 06:23:31 +0700

    Add default page for demo

--- timerproctor/src/App.js
+++ timerproctor/src/App.js
@@ -4,6 +4,7 @@ import './styles/globals.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ExamPage from './pages/exams/[id].js'
 import store, { StoreContext } from './stores'
+import DemoPage from './pages/demo'
 import ProctorDemoPage from './pages/proctorDemo'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/exams/:id" component={ExamPage} />
+          <Route exact path="/" component={DemoPage} />
           <Route exact path="/proctordemo" component={ProctorDemoPage} />
         </Switch>
       </Router>
---
+++ timerproctor/src/pages/demo.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { Button, Space } from 'antd'
+import DefaultLayout from '../layouts/default'
+
+const LargeButton = styled(Button)`
+  transform: scale(2.0);
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 100px;
+`
+
+const DemoPage = () => (
+  <DefaultLayout>
+    <Link to="/exams/5f991c780953aa4110686e76">
+      <LargeButton type="primary" size="large" ghost>Exam Demo</LargeButton>
+    </Link>
+    <Link to="/proctordemo">
+      <LargeButton type="primary" size="large" ghost>Proctor Demo</LargeButton>
+    </Link>
+  </DefaultLayout>
+)
+
+export default DemoPage
