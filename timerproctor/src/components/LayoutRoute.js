c2b97d62d1aa8fa588702200440f61719b8a901e
Anawin W <anawinwz@users.noreply.github.com>
2020-10-29 23:22:47 +0700

    Add LayoutRoute component

---
+++ timerproctor/src/components/LayoutRoute.js
@@ -0,0 +1,10 @@
+import { Route } from 'react-router-dom'
+
+const LayoutRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => <Layout><Component {...props} /></Layout>}
+  />
+)
+
+export default LayoutRoute
