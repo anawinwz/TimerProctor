b04161f42a3c7313ac0a7363fe9023a69b431d65
Anawin W <anawinwz@users.noreply.github.com>
2020-12-16 18:33:06 +0700

    Add testtakers tab bar

--- timerproctor/src/pages/admin/exams/[id]/overview.js
+++ timerproctor/src/pages/admin/exams/[id]/overview.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Tabs } from 'antd'
 
 import demoExam from '../../../../assets/demoExam.json'
 
@@ -6,6 +7,8 @@ import ContentBox from '../../../../components/admin/ContentBox'
 import ExamTitle from '../../../../components/admin/ExamTitle'
 import ExamStatusControls from '../../../../components/admin/ExamStatusControls'
 import ExamDescription from '../../../../components/admin/ExamDesciption'
+
+const { TabPane } = Tabs
 
 const ExamOverview = () => {
   const [exam, setExam] = useState(demoExam)
@@ -15,6 +18,13 @@ const ExamOverview = () => {
       <ExamTitle exam={exam} />
       <ExamStatusControls exam={exam} />
       <ExamDescription exam={exam} />
+      <Tabs centered>
+        <TabPane key="all" tab="ทั้งหมด" />
+        <TabPane key="waiting" tab="รออนุมัติเข้าสอบ" />
+        <TabPane key="approved" tab="รอเข้าสอบ" />
+        <TabPane key="doing" tab="ยังไม่ส่งคำตอบ" />
+        <TabPane key="submitted" tab="ส่งคำตอบแล้ว" />
+      </Tabs> 
     </ContentBox>
   )
 }
