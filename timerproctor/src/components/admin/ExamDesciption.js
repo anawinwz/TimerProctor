83291c6688881f2366edc0f57119fa46bda0826d
Anawin W <anawinwz@users.noreply.github.com>
2020-12-16 18:32:30 +0700

    Add top margin to ExamDescription

--- timerproctor/src/components/admin/ExamDesciption.js
+++ timerproctor/src/components/admin/ExamDesciption.js
@@ -5,11 +5,11 @@ const { Item } = Descriptions
 
 const ExamDescription = ({ exam }) => {
   return (
-    <Descriptions>
+    <Descriptions style={{ marginTop: '10px' }}>
       <Item label="จำกัดเวลาทำ">{ exam.timer.duration } นาที</Item>
       <Item label="แสดงนาฬิกาจับเวลา">{ exam.timer.isShow ? 'ใช่' : 'ไม่ใช่' }</Item>
       <Item label="ตรวจสอบบัตรประจำตัว">{ idCheckModes[exam.authentication.idCheckMode ] }</Item>
-      <Item label="เริ่มการสอบเมื่อ"></Item>
+      <Item label="เริ่มการสอบเมื่อ">{ exam.timeWindow.realtime.startedAt }</Item>
       <Item label="กรรมการคุมสอบขณะนี้ (0)"></Item>
     </Descriptions>
   )
