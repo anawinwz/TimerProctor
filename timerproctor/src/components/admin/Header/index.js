e80d0b9c00f176c3bf844d2de22bf893c31ab320
Anawin W <anawinwz@users.noreply.github.com>
2020-12-24 14:57:51 +0700

    Apply user-select and link to header logo

--- timerproctor/src/components/admin/Header/index.js
+++ timerproctor/src/components/admin/Header/index.js
@@ -1,4 +1,5 @@
 import { Layout, Row, Col } from 'antd'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import UserPopover from './UserPopover'
 
@@ -7,15 +8,17 @@ const HeaderBar = styled(Layout.Header)`
   padding-bottom: 0;
   background-color: white;
   .logo {
+    user-select: none;
     font-weight: bold;
     font-size: 24px;
+    color: black;
   }
 `
 
 const Header = () => (
   <HeaderBar>
     <Row justify="space-between">
-      <Col><div className="logo">TimerProctor</div></Col>
+      <Col><Link className="logo" to="/admin">TimerProctor</Link></Col>
       <Col>
         <UserPopover user={{ name: 'teacher@ku.th', role: { name: 'อาจารย์ผู้สอน' } }} />
       </Col>
