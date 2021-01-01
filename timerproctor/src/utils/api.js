5ee76e07a8871549fee7c3705da4ae09dd5b4d18
Anawin W <anawinwz@users.noreply.github.com>
2021-01-02 00:50:18 +0700

    Add fetchAPIwithToken

--- timerproctor/src/utils/api.js
+++ timerproctor/src/utils/api.js
@@ -1,10 +1,34 @@
+import { getToken } from './token'
+
+const baseUrl = `http://localhost:5000`
+
 export const fetchAPI = async (endpoint, body) => {
   try {
-    const url = `http://localhost:5000${endpoint}`
+    const url = `${baseUrl}${endpoint}`
     const res = await fetch(url, !body ? {} : {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    
+    const data = await res.json()
+    return data
+  } catch (err) {
+    throw err
+  }
+}
+
+
+export const fetchAPIwithToken = async (endpoint, body, token) => {
+  try {
+    const url = `${baseUrl}${endpoint}`
+    const res = await fetch(url, !body ? {} : {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Token': token || getToken(),
       },
       body: JSON.stringify(body)
     })
