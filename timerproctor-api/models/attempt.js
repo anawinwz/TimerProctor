c76b8953a4fbd9a7fa2b022351c10bcb0f146132
Anawin W <anawinwz@users.noreply.github.com>
2021-01-07 18:26:14 +0700

    Add Attempt schema

---
+++ timerproctor-api/models/attempt.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const schema = new Schema(
+  {
+    _id: Schema.Types.ObjectId,
+    exam: { type: Schema.Types.ObjectId, ref: 'Exam' },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    status: {
+      type: String,
+      enum: ['login', 'authenticate', 'attempt', 'completed']
+    },
+    idCheck: {
+      accepted: { type: Boolean, default: false },
+      reason: { type: String, default: '' },
+      checker: { type: Schema.Types.ObjectId, ref: 'User' },
+      checkedAt: { type: Date }
+    }
+  }
+)
+
+export default mongoose.model('Attempt', schema)
