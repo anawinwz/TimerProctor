1cce2d674d140b4f091538079aaae4c490777977
Anawin W <anawinwz@users.noreply.github.com>
2021-01-07 21:11:51 +0700

    Add CORS for Socket.IO server testing

--- timerproctor-api/index.js
+++ timerproctor-api/index.js
@@ -13,7 +13,12 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+})
 
 io.of(ioNamespace)
   .on('connection', socketioJwt.authorize({
