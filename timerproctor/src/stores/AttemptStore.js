3a453766efee15e20a1b2c0b3eaaa15735b93938
Anawin W <anawinwz@users.noreply.github.com>
2021-01-07 19:58:55 +0700

    Implement getAttempt action

--- timerproctor/src/stores/AttemptStore.js
+++ timerproctor/src/stores/AttemptStore.js
@@ -1,17 +1,27 @@
 import { action, observable } from 'mobx'
+import { fetchAPIwithToken } from '../utils/api'
 class AttemptStore {
-  @observable state = 'login'
+  @observable status = 'login'
   @observable socketToken = ''
 
   constructor(rootStore) {
     this.rootStore = rootStore
+    this.examStore = this.rootStore.ExamStore
+    this.idCheckStore = this.rootStore.IDCheckStore
   }
 
   @action
-  getState() {}
+  getAttempt() {
+    const examId = this.examStore.id
+    
+    const { status: resStatus, message, payload } = await fetchAPIwithToken(`/exams/${examId}/attempt`, {})
+    if (!resStatus || resStatus !== 'ok') throw new Error(message || 'ไม่สามารถขอเริ่มการสอบได้')
 
-  @action
-  getSocketToken() {}
+    const { status, socketToken, idCheck: { accepted, reason } } = payload
+    this.status = status
+    this.socketToken = socketToken
+    this.idCheckStore.setResult(accepted, reason)
+  }
 }
 
 export default AttemptStore
