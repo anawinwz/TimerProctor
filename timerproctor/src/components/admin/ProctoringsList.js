3f6f27c42fa88a2d80d3348880db3b4674138b40
Anawin W <anawinwz@users.noreply.github.com>
2021-01-08 19:55:33 +0700

    Load real proctorings from API

--- timerproctor/src/components/admin/ProctoringsList.js
+++ timerproctor/src/components/admin/ProctoringsList.js
@@ -1,4 +1,7 @@
-import { Typography } from 'antd'
+import { useState, useEffect } from 'react'
+import { message, Typography } from 'antd'
+
+import { fetchAPIwithToken } from '../../utils/api'
 
 import ContentBox from './ContentBox'
 import ExamsListTable from './ExamsListTable'
@@ -6,10 +9,23 @@ import ExamsListTable from './ExamsListTable'
 const { Title } = Typography
 
 const ProctoringsList = () => {
+  const [proctorings, setProctorings] = useState([])
+
+  useEffect(async () => {
+    try {
+      const res = await fetchAPIwithToken('/proctorings')
+      const { status, payload, message } = res
+      if (status === 'success') setProctorings(payload.proctorings)
+      else throw new Error(message || 'เกิดข้อผิดพลาดในการโหลดข้อมูลการคุมสอบของฉัน')
+    } catch {
+      message.error('เกิดข้อผิดพลาดในการโหลดข้อมูลการคุมสอบของฉัน')
+    }
+  }, [])
+
   return (
     <ContentBox>
       <Title level={3}>การคุมสอบของฉัน</Title>
-      <ExamsListTable />
+      <ExamsListTable dataSource={proctorings} />
     </ContentBox>
   ) 
 }
