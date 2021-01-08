4f261f81b098d7a2be22a82f28a6b4a06d9c4575
Anawin W <anawinwz@users.noreply.github.com>
2021-01-09 00:33:08 +0700

    Connect testers API

--- timerproctor/src/components/admin/ExamTesters/index.js
+++ timerproctor/src/components/admin/ExamTesters/index.js
@@ -1,35 +1,44 @@
-import { useState, useCallback, useMemo } from 'react'
-import { Row, Radio, Pagination } from 'antd'
+import { useState, useCallback, useEffect } from 'react'
+import { Row, Radio, Pagination, message } from 'antd'
 import { CheckSquareOutlined, TableOutlined, UnorderedListOutlined } from '@ant-design/icons'
 
 import ApproveView from './views/approve'
 import GridView from './views/grid'
 import ListView from './views/list'
-import { getTesters } from '../../../utils/demo'
-
-const demoTesters = getTesters(16)
+import { fetchAPIwithToken } from '../../../utils/api'
 
 const ExamTesters = ({ status }) => {
   const [viewMode, setViewMode] = useState(status === 'authenticate' ? 'approve' : 'grid')
   const [page, setPage] = useState(1)
   const [pageSize, setPageSize] = useState(6)
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [testers, setTesters] = useState([])
+
   const changePage = useCallback(page => {
     setPage(page)
-  }, [setPage])
+  }, [])
   const changePageSize = useCallback((current, pageSize) => {
     setPageSize(pageSize)
-  }, [setPageSize])
-
+  }, [])
   const changeViewMode = useCallback(e => {
     setViewMode(e.target.value)
-  }, [setViewMode])
+  }, [])
 
-  const testers = demoTesters
-  const filteredTesters = useMemo(() => {
-    if (status === 'all') return testers
-    return testers.filter(tester => tester.status === status)
-  }, [testers])
-  const pagedTesters = filteredTesters.slice(0 + pageSize * (page - 1), 0 + pageSize * page) 
+  useEffect(async () => {
+    setIsLoading(true)
+    try {
+      const res = await fetchAPIwithToken(`/exams/5f991c780953aa4110686e76/testers?status=${status}&page=${page}&pageSize=${pageSize}`)
+      if (res.status === 'success') {
+        setTesters(res.payload.testers)
+        setIsLoading(false)
+      } else {
+        throw new Error(res.message || 'เกิดข้อผิดพลาดในการโหลดข้อมูลหน้านี้')
+      }
+    } catch (err) {
+      message.error(err.message || 'เกิดข้อผิดพลาดในการโหลดข้อมูลหน้านี้')
+    }
+  }, [status, page, pageSize])
 
   return (
     <>
@@ -42,7 +51,7 @@ const ExamTesters = ({ status }) => {
           style={{ marginBottom: '8px' }}
         >
           { 
-            status === 'authenticate' && 
+            status === 'loggedin' && 
             <Radio.Button value="approve"><CheckSquareOutlined /> โหมดอนุมัติ (1 จอ)</Radio.Button>
           }
           <Radio.Button value="grid"><TableOutlined /> ตารางภาพ</Radio.Button>
@@ -50,20 +59,21 @@ const ExamTesters = ({ status }) => {
         </Radio.Group>
       </Row>
       <Row justify="center" gutter={4}>
-        { viewMode === 'approve' && <ApproveView testers={pagedTesters} /> }
-        { viewMode === 'grid' && <GridView pageSize={pageSize} testers={pagedTesters} /> }
-        { viewMode === 'list' && <ListView pageSize={pageSize} testers={pagedTesters} /> }
+        { viewMode === 'approve' && <ApproveView testers={testers} /> }
+        { viewMode === 'grid' && <GridView pageSize={pageSize} testers={testers} /> }
+        { viewMode === 'list' && <ListView pageSize={pageSize} testers={testers} /> }
       </Row>
       { viewMode !== 'approve' &&
         <Row justify="center">
           <Pagination
             current={page}
-            total={filteredTesters.length}
+            total={1}
             onChange={changePage}
             showSizeChanger
             pageSize={pageSize}
             onShowSizeChange={changePageSize}
             pageSizeOptions={[6, 12, 16, 20, 24]}
+            disabled={isLoading}
           />
         </Row>
       }
