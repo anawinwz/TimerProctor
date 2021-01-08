d55f70091274239f8d41790c9ecbea09afc6bb6b
Anawin W <anawinwz@users.noreply.github.com>
2021-01-08 19:54:33 +0700

    Load real exams from API

--- timerproctor/src/components/admin/ExamsList.js
+++ timerproctor/src/components/admin/ExamsList.js
@@ -1,4 +1,7 @@
-import { Typography } from 'antd'
+import { useState, useEffect } from 'react'
+import { message, Typography } from 'antd'
+
+import { fetchAPIwithToken } from '../../utils/api'
 
 import ContentBox from './ContentBox'
 import AddExamButton from './AddExamButton'
@@ -7,11 +10,24 @@ import ExamsListTable from './ExamsListTable'
 const { Title } = Typography
 
 const ExamsList = ({ pageSize = 5 }) => {
+  const [exams, setExams] = useState([])
+
+  useEffect(async () => {
+    try {
+      const res = await fetchAPIwithToken('/exams')
+      const { status, payload, message } = res
+      if (status === 'success') setExams(payload.exams)
+      else throw new Error(message || 'เกิดข้อผิดพลาดในการโหลดข้อมูลการสอบของฉัน')
+    } catch {
+      message.error('เกิดข้อผิดพลาดในการโหลดข้อมูลการสอบของฉัน')
+    }
+  }, [])
+
   return (
     <ContentBox>
       <Title level={3}>การสอบของฉัน</Title>
       <AddExamButton />
-      <ExamsListTable pageSize={pageSize} />
+      <ExamsListTable pageSize={pageSize} dataSource={exams} />
     </ContentBox>
   ) 
 }
