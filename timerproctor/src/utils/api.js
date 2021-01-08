999566d660ca0d6f6c36b3f2c3609038a8ddf14b
Anawin W <anawinwz@users.noreply.github.com>
2021-01-08 19:42:53 +0700

    Fix no token on fetchAPIwithToken get method

--- timerproctor/src/utils/api.js
+++ timerproctor/src/utils/api.js
@@ -24,14 +24,19 @@ export const fetchAPI = async (endpoint, body) => {
 export const fetchAPIwithToken = async (endpoint, body, token) => {
   try {
     const url = `${baseUrl}${endpoint}`
-    const res = await fetch(url, !body ? {} : {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Access-Token': token || getToken(),
-      },
-      body: JSON.stringify(body)
-    })
+    const res = await fetch(url, 
+      !body ?
+      {
+        headers: { 'X-Access-Token': token || getToken() },
+      } :
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Access-Token': token || getToken(),
+        },
+        body: JSON.stringify(body)
+      })
     
     const data = await res.json()
     return data
