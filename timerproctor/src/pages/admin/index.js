28d4dfaa3ebd472abf75ea1b7310feb0b5104c9d
Anawin W <anawinwz@users.noreply.github.com>
2021-01-09 16:02:01 +0700

    Add /admin page redirect by auth state

--- timerproctor/src/pages/admin/index.js
+++ timerproctor/src/pages/admin/index.js
@@ -1,4 +1,4 @@
-import { Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 import { observer } from 'mobx-react'
 import { useStore } from '~/stores/admin'
@@ -14,6 +14,12 @@ import DashboardPage from './dashboard'
 import ExamsPage from './exams/index'
 import ExamPage from './exams/[id]'
 
+const LoginDashboardRedirect = observer(() => {
+  const { AuthStore: { isLoggedIn } } = useStore()
+  if (isLoggedIn) return <Redirect to="/admin/dashboard" />
+  return <Redirect to="/admin/login" />
+})
+
 const AdminPage = () => {
   const { AppStore: { hydrated } } = useStore()
   if (!hydrated) return <Loading />
@@ -23,6 +29,7 @@ const AdminPage = () => {
       <LayoutRoute exact path="/admin/dashboard" component={DashboardPage} layout={AdminLayout} />
       <LayoutRoute exact path="/admin/exams" component={ExamsPage} layout={AdminLayout} />
       <LayoutRoute path="/admin/exams/:id" component={ExamPage} layout={AdminLayout} />
+      <Route exact path="/admin" component={LoginDashboardRedirect} />
     </Switch>
   )
 }
