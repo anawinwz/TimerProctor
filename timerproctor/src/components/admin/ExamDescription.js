18509e6434f54fef78edc44017dfe50244a7d8d4
Anawin W <anawinwz@users.noreply.github.com>
2021-01-09 19:17:28 +0700

    Display exam descriptions by timeWindow mode

--- timerproctor/src/components/admin/ExamDescription.js
+++ timerproctor/src/components/admin/ExamDescription.js
@@ -1,16 +1,23 @@
 import { observer } from 'mobx-react'
 import { Descriptions } from 'antd'
 import { idCheckModes } from '~/utils/const'
+import { rangeStr, shortDateStr } from '~/utils/date'
 
 const { Item } = Descriptions
 
-const ExamDescription = ({ exam }) => {
+const ExamDescription = ({ exam = {} }) => {
+  const { timeWindow, timer, authentication } = exam
   return (
     <Descriptions style={{ marginTop: '10px' }}>
-      <Item label="จำกัดเวลาทำ">{ exam?.timer?.duration } นาที</Item>
-      <Item label="แสดงนาฬิกาจับเวลา">{ exam?.timer?.isShow ? 'ใช่' : 'ไม่ใช่' }</Item>
-      <Item label="ตรวจสอบบัตรประจำตัว">{ idCheckModes[exam?.authentication?.idCheckMode ] }</Item>
-      <Item label="เริ่มการสอบเมื่อ">{ exam?.timeWindow?.realtime?.startedAt }</Item>
+      <Item label="จำกัดเวลาทำ">{ timer?.duration } นาที</Item>
+      <Item label="แสดงนาฬิกาจับเวลา">{ timer?.isShow ? 'ใช่' : 'ไม่ใช่' }</Item>
+      <Item label="ตรวจสอบบัตรประจำตัว">{ idCheckModes[authentication?.idCheckMode ] }</Item>
+      { 
+        timeWindow?.mode === 'realtime' ?
+        <Item label="เริ่มการสอบเมื่อ">{ shortDateStr(timeWindow?.realtime?.startedAt) }</Item>
+        :
+        <Item label="กำหนดการสอบ">{ rangeStr(timeWindow?.schedule?.startDate, timeWindow?.schedule?.endDate) }</Item>
+      }
       <Item label="กรรมการคุมสอบขณะนี้ (0)"></Item>
     </Descriptions>
   )
