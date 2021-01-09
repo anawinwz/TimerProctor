2c4213a41e72ff77d5f98ddd6abc4c11d8d6b09b
Anawin W <anawinwz@users.noreply.github.com>
2021-01-09 15:35:56 +0700

    Correct babel-plugin-root-import config

--- timerproctor/config-overrides.js
+++ timerproctor/config-overrides.js
@@ -3,5 +3,12 @@ const { override, addBabelPlugins } = require('customize-cra')
 module.exports = override(
   addBabelPlugins(
     ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      'babel-plugin-root-import', 
+      {
+        rootPathSuffix: './src/',
+        rootPathPrefix: '~/'
+      }
+    ],
   )
 )
