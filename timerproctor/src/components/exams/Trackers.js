33d29353dce407932b366ddae891b6e491708afd
Anawin W <anawinwz@users.noreply.github.com>
2021-01-10 23:27:11 +0700

    Submit signals via sockets

--- timerproctor/src/components/exams/Trackers.js
+++ timerproctor/src/components/exams/Trackers.js
@@ -1,13 +1,22 @@
-import { message } from 'antd'
+
 import { useCallback } from 'react'
+import { message } from 'antd'
+import { observer } from 'mobx-react-lite'
+import { useStore } from '~/stores/index'
 import FaceTracker from './FaceTracker'
 import WindowTracker from './WindowTracker'
 
 const Trackers = () => {
+  const { SocketStore: socketStore } = useStore()
   const signal = useCallback((sigData) => {
-    console.log(sigData)
-    if (sigData.msg)
+    if (sigData.msg) {
       message.warning(<>{ sigData.msg }<br />อาจารย์ผู้สอน/กรรมการคุมสอบจะได้รับการแจ้งเตือนนี้</>)
+    }
+    delete sigData.msg
+    
+    socketStore.socket.emit('signal', sigData, data => {
+      if (data.err) console.log('signal error')
+    })
   }, [])
   return (
     <>
@@ -17,4 +26,4 @@ const Trackers = () => {
   )
 }
 
-export default Trackers
+export default observer(Trackers)
