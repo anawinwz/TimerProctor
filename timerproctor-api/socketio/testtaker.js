36d29a2500bed555ab3e99b036ba10a5213903d1
Anawin W <anawinwz@users.noreply.github.com>
2021-01-10 23:15:46 +0700

    Listen idCheck and snapshot event from testtakers

--- timerproctor-api/socketio/testtaker.js
+++ timerproctor-api/socketio/testtaker.js
@@ -1,2 +1,53 @@
+import Attempt from '../models/attempt'
+import AttemptEvent from '../models/attemptEvent'
+import { getExamIdFromSocket, getExamNsp } from '../utils/helpers'
+
 export default (socket, user = {}) => {
+  const socketInfo = socket?.attempt
+  const examId = getExamIdFromSocket(socket)
+  socket.on('idCheck', async (data, callback) => {
+    const { image, timestamp } = data
+    if (!socketInfo || !image || !timestamp) {
+      return callback({ err: true })
+    }
+
+    let attempt = await Attempt.findById(socketInfo.id)
+    attempt.idCheck.photoURL = image
+    attempt.idCheck.timestamp = timestamp
+    attempt.idCheck.accepted = false
+    attempt.idCheck.reason = ''
+    attempt.idCheck.checker = ''
+    attempt.idCheck.checkedAt = null
+    attempt = await attempt.save()
+
+    getExamNsp(examId).to('proctor').emit('idCheckRequest', {
+      id: socketInfo.id,
+      name: user?.info?.displayName,
+      image: image
+    })
+    
+    callback({})
+  })
+  socket.on('snapshot', (data, callback) => {
+    const { image, facesCount, timestamp } = data
+    if (!socketInfo || !image || !facesCount || !timestamp) {
+      return callback({ err: true })
+    }
+
+    const newEvent = new AttemptEvent({
+      attempt: socketInfo.id,
+      timestamp,
+      type: 'snapshot',
+      info: { facesCount },
+      evidence: {
+        type: 'photo',
+        url: image
+      }
+    })
+
+    newEvent.save(err => {
+      if (err) return callback({ err: true })
+      callback({ err: false })
+    })
+  })
 }
