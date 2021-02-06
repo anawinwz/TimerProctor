cfe7d8e97714b85233a2732eff81dda2b5d0b90d
Anawin W <anawinwz@users.noreply.github.com>
2021-02-06 18:15:24 +0700

    Add includes and notIncludes validators

--- timerproctor/src/utils/form.js
+++ timerproctor/src/utils/form.js
@@ -3,13 +3,20 @@ export const toOptions = (object) => Object.keys(object).map(value => ({ label: 
 export const validators = {
   equal: (values) => (rule, value) => 
     value == values[0] ? 
-    Promise.resolve() : Promise.reject(rule.message || `ค่าต้องเท่ากับ ${values[0]}`),
+    Promise.resolve() : Promise.reject(rule.message || `ต้องเป็นตัวเลขที่เท่ากับ ${values[0]}`),
   nEqual: (values) => (rule, value) => 
     value != values[0] ? 
-    Promise.resolve() : Promise.reject(rule.message || `ค่าต้องไม่เท่ากับ ${values[0]}`),
+    Promise.resolve() : Promise.reject(rule.message || `ต้องเป็นตัวเลขที่ไม่เท่ากับ ${values[0]}`),
   notBetween: (values) => (rule, value) => 
     value <= values[0] || value >= values[1] ? 
-    Promise.resolve() : Promise.reject(rule.message || `ค่าต้องไม่อยู่ระหว่าง ${values[0]} - ${values[1]}`)
+    Promise.resolve() : Promise.reject(rule.message || `ต้องเป็นตัวเลขที่ไม่อยู่ระหว่าง ${values[0]} - ${values[1]}`),
+  
+  includes: (values) => (rule, value) => 
+    value.includes(values[0]) ? 
+    Promise.resolve() : Promise.reject(rule.message || `ต้องมี ${values[0]}`),
+  notIncludes: (values) => (rule, value) => 
+    !value.includes(values[0]) ? 
+    Promise.resolve() : Promise.reject(rule.message || `ต้องไม่มี ${values[0]}`),
 }
 
 export const validateMessages = {
