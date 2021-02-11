1aaa4a63a48615724d74aac7d3619f82e6ee5293
Anawin W <anawinwz@users.noreply.github.com>
2021-02-12 01:56:57 +0700

    Fix invalid import

--- timerproctor-api/middlewares/attempt.js
+++ timerproctor-api/middlewares/attempt.js
@@ -1,4 +1,4 @@
-import Attempt from '../models/exam'
+import Attempt from '../models/attempt'
 import { jsonResponse } from '../utils/helpers'
 
 export const populateAttempt = async (req, res, next) => {
@@ -8,7 +8,7 @@ export const populateAttempt = async (req, res, next) => {
       _id: req.params.testerId,
       exam: exam._id
     })
-
+    console.log(req.params.testerId)
     if (!attempt) return res.json(jsonResponse('error', 'ไม่พบผู้เข้าสอบดังกล่าว'))
     req.attempt = attempt
     return next()
