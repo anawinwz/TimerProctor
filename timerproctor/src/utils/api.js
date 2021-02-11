e8918bb1547d59d13915abfd7ec0884d04141d1d
Anawin W <anawinwz@users.noreply.github.com>
2021-02-11 18:33:27 +0700

    Set API baseURL via environment variable

--- timerproctor/src/utils/api.js
+++ timerproctor/src/utils/api.js
@@ -6,7 +6,7 @@ export class APIFailedError extends Error {
   }
 }
 
-export const baseUrl = `http://localhost:5000`
+export const baseUrl = process.env.REACT_APP_API_BASEURL || 'http://localhost:5000'
 
 export const fetchAPI = async (endpoint, body) => {
   try {
