519400263317ad4bf77fe30985f63d16b5446dff
Anawin W <anawinwz@users.noreply.github.com>
2021-02-16 12:57:41 +0700

    Add onlyDuring exam middleware

--- timerproctor-api/middlewares/exam.js
+++ timerproctor-api/middlewares/exam.js
@@ -1,5 +1,6 @@
 import Exam from '../models/exam'
 import { jsonResponse } from '../utils/helpers'
+import dayjs from '../utils/dayjs'
 
 export const populateExam = async (req, res, next) => {
   try {
@@ -15,4 +16,18 @@ export const populateExam = async (req, res, next) => {
 export const onlyExamOwner = async (req, res, next) => {
   if (String(req.exam.owner) === String(req.user._id)) return next()
   return res.json(jsonResponse('failed', 'Access Denied.'))
+}
+
+export const onlyDuringExam = async (req, res, next) => {
+  const { timeWindow } = req.exam
+  const { mode, realtime, schedule } = timeWindow
+  
+  let isStarted = false
+  switch (mode) {
+    case 'realtime': isStarted = realtime.status === 'started'; break
+    case 'schedule': isStarted = dayjs().isBetween(schedule.startAt, schedule.endDate, '[]'); break
+  }
+
+  if (isStarted) return next()
+  return res.json(jsonResponse('failed', 'ขณะนี้ยังไม่ถึงเวลาเข้าสอบ'))
 }
