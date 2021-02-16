49303a4616baebd50ae0ca69ae2fa7560a402183
Anawin W <anawinwz@users.noreply.github.com>
2021-02-17 03:06:28 +0700

    Add navigation guard to /exams/:id/authenticate

--- timerproctor/src/pages/exams/[id]/authenticate.js
+++ timerproctor/src/pages/exams/[id]/authenticate.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
+import { useStore } from '~/stores/index'
+
 import AuthenCard from '~/components/exams/AuthenCard'
 
-const AuthenPage = () => {
+const AuthenPage = ({ history }) => {
+  const { ExamStore: exam, AuthStore: { isLoggedIn } } = useStore()
+
+  useEffect(() => {
+    if (!isLoggedIn) history.replace(`/exams/${exam.id}`)
+  }, [])
+  
   return (
     <AuthenCard />
   )
 }
 
-export default AuthenPage
+export default observer(AuthenPage)
