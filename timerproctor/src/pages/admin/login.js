3091da70a8e8ddf678d3a197096edca5675da05f
Anawin W <anawinwz@users.noreply.github.com>
2021-02-27 01:08:44 +0700

    Remove unused useHistory hook

--- timerproctor/src/pages/admin/login.js
+++ timerproctor/src/pages/admin/login.js
@@ -1,4 +1,4 @@
-import { Redirect, useHistory } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import { useStore } from '~/stores/admin'
 
@@ -6,7 +6,6 @@ import IntroLogin from '~/components/admin/IntroLogin'
 import useAppTitle from '~/hooks/useAppTitle'
 
 const AdminLogin = () => {
-  const { location } = useHistory()
   const { AuthStore: { isLoggedIn } } = useStore()
   useAppTitle('เข้าสู่ระบบ', { admin: true })
 
