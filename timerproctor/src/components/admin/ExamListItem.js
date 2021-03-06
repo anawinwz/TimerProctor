44821bf3970b4f2e32dba7f5974065c0a2086d0f
Anawin W <anawinwz@users.noreply.github.com>
2021-03-06 20:35:20 +0700

    Separate ExamListItem component

---
+++ timerproctor/src/components/admin/ExamListItem.js
@@ -0,0 +1,41 @@
+import { List, Typography } from 'antd'
+import { CalendarOutlined, } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+import StatusTag from './StatusTag'
+
+import { rangeStr, shortDateStr } from '~/utils/date'
+
+const ExamListItem = ({ exam }) => {
+  const { _id, name, status, timeWindow, updatedAt, createdAt } = exam
+
+  let str_timeWindow = ''
+  if (status === 'unset' || !timeWindow) str_timeWindow = '-'
+  else if (timeWindow.mode === 'realtime') str_timeWindow = 'ตามเวลาจริง'
+  else str_timeWindow = rangeStr(timeWindow.schedule?.startDate, timeWindow.schedule?.endDate)
+
+  return (
+    <List.Item>
+      <List.Item.Meta
+        title={
+          <Link to={`/admin/exams/${_id}/${status === 'unset' ? 'settings' : 'overview'}`}>
+            <Typography.Title level={5}>{ name }</Typography.Title>
+          </Link>
+        }
+        description={
+          <>
+            <StatusTag className="d-block" status={status} />
+            <Typography.Text className="d-block">
+              <CalendarOutlined /> { str_timeWindow }
+            </Typography.Text>
+            <Typography.Text className="d-block" type="secondary">
+              { shortDateStr(updatedAt || createdAt) }
+            </Typography.Text>
+          </>
+        }
+      />
+    </List.Item>
+  )
+}
+
+export default ExamListItem
--- timerproctor/src/components/admin/ExamsList.js
+++ timerproctor/src/components/admin/ExamsList.js
@@ -1,46 +1,12 @@
 import { useCallback } from 'react'
-import { List, Typography } from 'antd'
-import { CalendarOutlined, LoadingOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import { List } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
 
 import { observer } from 'mobx-react-lite'
-
-import StatusTag from './StatusTag'
-
-import { rangeStr, shortDateStr } from '~/utils/date'
+import ExamListItem from './ExamListItem'
 
 const ExamsList = ({ pageSize = 5, loading = false, dataSource = [], header = null }) => {
-  const renderExam = useCallback(exam => {
-    const { _id, name, status, timeWindow, updatedAt, createdAt } = exam
-
-    let str_timeWindow = ''
-    if (status === 'unset' || !timeWindow) str_timeWindow = '-'
-    else if (timeWindow.mode === 'realtime') str_timeWindow = 'ตามเวลาจริง'
-    else str_timeWindow = rangeStr(timeWindow.schedule?.startDate, timeWindow.schedule?.endDate)
-  
-    return (
-      <List.Item>
-        <List.Item.Meta
-          title={
-            <Link to={`/admin/exams/${_id}/${status === 'unset' ? 'settings' : 'overview'}`}>
-              <Typography.Title level={5}>{ name }</Typography.Title>
-            </Link>
-          }
-          description={
-            <>
-              <StatusTag className="d-block" status={status} />
-              <Typography.Text className="d-block">
-                <CalendarOutlined /> { str_timeWindow }
-              </Typography.Text>
-              <Typography.Text className="d-block" type="secondary">
-                { shortDateStr(updatedAt || createdAt) }
-              </Typography.Text>
-            </>
-          }
-        />
-      </List.Item>
-    )
-  }, [])
+  const renderExam = useCallback(exam => <ExamListItem exam={exam} />, [])
 
   return (
     <List
