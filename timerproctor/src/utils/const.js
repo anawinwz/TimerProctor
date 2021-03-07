5c3f950b9af43cef8a1d6711c7fdc675b155f40f
Anawin W <anawinwz@users.noreply.github.com>
2021-03-07 23:52:16 +0700

    Set default string for nl2br

--- timerproctor/src/utils/const.js
+++ timerproctor/src/utils/const.js
@@ -1,4 +1,4 @@
-export const nl2br = str => str.replace(/\n/g, `<br />`)
+export const nl2br = (str = '') => str.replace(/\n/g, `<br />`)
 
 export const timeWindowModes = {
   realtime: 'ให้สัญญาณเริ่ม-สิ้นสุดด้วยตนเอง',
