6fc37df7730da01b7660f27b4ff3b45f4a970f98
Anawin W <anawinwz@users.noreply.github.com>
2021-03-08 04:21:46 +0700

    [razzle] Add template support to SSR

--- timerproctor/src/server.js
+++ timerproctor/src/server.js
@@ -3,32 +3,28 @@ import React from 'react'
 import express from 'express'
 import { renderToString } from 'react-dom/server'
 
+const fs = require('fs')
+const template = fs.readFileSync('src/_app.html', 'utf-8')
+
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
 export const renderApp = (req, res) => {
   const context = {}
   const markup = renderToString(<App context={context} />)
 
-  const html =
-    // prettier-ignore
-    `<!doctype html>
-  <html lang="">
-  <head>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta charSet='utf-8' />
-      <title>Welcome to Razzle</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      ${
-      assets.client.css
-        ? `<link rel="stylesheet" href="${assets.client.css}">`
-        : ''
-      }
-  </head>
-  <body>
-      <div id="root">${markup}</div>
-      <script src="${assets.client.js}" defer crossorigin></script>
-  </body>
-</html>`
+  const html = template
+    .replace(/%PUBLIC_URL%/g, '')
+    .replace(
+      /<!--%ASSETS_CLIENT_CSS%-->/g,
+      assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''
+    )
+    .replace(/%ASSETS_CLIENT_CSS%/g, assets.client.css)
+    .replace('<!--%MARKUP%-->', markup)
+    .replace(
+      /<!--%ASSETS_CLIENT_CSS%-->/g,
+      `<script src="${assets.client.js}" defer crossorigin></script>`
+    )
+    .replace(/%ASSETS_CLIENT_JS%/g, assets.client.js)
 
   return { html, status: context.status }
 }
