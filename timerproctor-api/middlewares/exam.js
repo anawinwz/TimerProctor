5777d9ca0db3834652c4db6c843f098ca79e7473
Anawin W <anawinwz@users.noreply.github.com>
2021-03-08 07:40:44 +0700

    Use notfound status instead

--- timerproctor-api/middlewares/exam.js
+++ timerproctor-api/middlewares/exam.js
@@ -6,7 +6,7 @@ import dayjs from '../utils/dayjs'
 export const populateExam = async (req, res, next) => {
   try {
     const exam = await Exam.findById(req.params.id)
-    if (!exam) return res.json(jsonResponse('failed', 'ไม่พบข้อมูลการสอบดังกล่าว'))
+    if (!exam) return res.json(jsonResponse('notfound', 'ไม่พบข้อมูลการสอบดังกล่าว'))
     req.exam = exam
     return next()
   } catch {
