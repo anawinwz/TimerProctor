ca4b110330ae1899296ef0b433149f73b0ec7604
Anawin W <anawinwz@users.noreply.github.com>
2021-03-21 22:29:32 +0700

    Hardcoded cookie domain

--- timerproctor-api/src/utils/const.js
+++ timerproctor-api/src/utils/const.js
@@ -7,9 +7,7 @@ export const cookieNames = {
   refreshToken_admin: 'tp__refreshToken_admin'
 }
 
-export const cookieDomain = process.env.COOKIE_DOMAIN || undefined
-
 export const defaultCookieOptions = {
   httpOnly: true,
-  domain: cookieDomain
+  domain: '.anawinwz.me'
 }
