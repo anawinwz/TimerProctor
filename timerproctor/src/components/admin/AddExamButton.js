5bbc8d0dec29f53332b8d962ce1a5ee44c927919
Anawin W <anawinwz@users.noreply.github.com>
2021-03-21 17:41:14 +0700

    Update AddExamButton modal content

--- timerproctor/src/components/admin/AddExamButton.js
+++ timerproctor/src/components/admin/AddExamButton.js
@@ -16,13 +16,13 @@ const AddExamButton = () => {
       <Modal title="เพิ่มการสอบ" visible={isShow} onOk={closeModal} onCancel={closeModal} cancelButtonProps={{ style: { display: 'none' } }}>
         คุณสามารถเพิ่มการสอบได้โดยใช้ Google Forms
         <ol>
+          <li><a href="https://workspace.google.com/marketplace/app/timerproctor/926658420781">ติดตั้งส่วนเสริม <b>TimerProctor</b></a> ลงในบัญชี Google ของคุณ</li>
           <li>ไปที่ <a href="https://forms.google.com/" target="_blank">Google Forms ของคุณ</a> และเปิดฟอร์มข้อสอบที่คุณต้องการเพิ่มขึ้นมา</li>
           <li>
             <p>คลิกที่สัญลักษณ์ <b>...</b>  &gt; เลือก <b>ส่วนเสริม / Add-ons</b></p>
             <img src={GoogleFormsMenu} />
           </li>
-          <li>เลือก <b>TimerProctor</b> &gt; คลิก <b>กำหนดค่า (Configure)</b></li>
-          <li>ติ๊ก <b>เปิดใช้งานส่วนเสริม</b></li>
+          <li>เลือก <b>TimerProctor</b> &gt; คลิก <b>สร้างเป็นการสอบของฉัน</b></li>
           <li>การสอบที่เชื่อมโยงกับฟอร์มดังกล่าวจะปรากฏขึ้นที่หน้านี้อัตโนมัติ</li>
         </ol>
       </Modal>
