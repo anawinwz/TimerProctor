910838a91e7fb51421da9587bb5b51a7a4958d0a
Anawin W <anawinwz@users.noreply.github.com>
2021-03-21 18:16:58 +0700

    Add email link login to admin

--- timerproctor/src/components/admin/IntroLogin.js
+++ timerproctor/src/components/admin/IntroLogin.js
@@ -1,13 +1,15 @@
-import { useCallback } from 'react'
+import { useEffect, useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Card, Space, Typography, message, Alert } from 'antd'
+import { Card, Space, Typography, Divider, message, Alert, Spin } from 'antd'
 import { observer } from 'mobx-react-lite'
 import { useStore } from '~/stores/admin'
 
+import { showModal } from '~/utils/modal'
 import { getNextURL, removeNextURL } from '~/utils/redirect'
 
 import Logo from '~/components/Logo'
 import GoogleLoginButton from '~/components/buttons/GoogleLoginButton'
+import EmailLinkLoginButton from '~/components/buttons/EmailLinkLoginButton'
 
 const IntroLogin = () => {
   const { AuthStore: auth } = useStore()
@@ -15,12 +17,41 @@ const IntroLogin = () => {
   const history = useHistory()
   const nextURL = getNextURL(history.location)
 
-  const login = useCallback(async method => {
-    try {
-      await auth.doAuthen(method)
+  const removeEmailParams = useCallback(() => {
+    const q = new URLSearchParams(history.location.search)
+    q.delete('apiKey')
+    q.delete('oobCode')
+    q.delete('mode')
+    q.delete('lang')
+    history.replace({ search: q.toString() })
+  }, [])
 
+  const enterDashboard = result => {
+    if (result === true) {
       if (nextURL) removeNextURL()
       history.replace(nextURL || '/admin/dashboard')
+    } else if (result?.message) {
+      showModal('info', 'ข้อความ', result.message, {
+        onOk: result?.onOk || (() => {})
+      })
+    }
+  }
+
+  useEffect(async () => {
+    try {
+      const result = await auth.doEmailCallback()
+      removeEmailParams()
+      enterDashboard(result)
+    } catch (err) {
+      removeEmailParams()
+      message.error(err.message)
+    }
+  }, [])
+
+  const login = useCallback(async method => {
+    try {
+      const result = await auth.doAuthen(method)
+      enterDashboard(result)
     } catch (err) {
       message.error(err.message)
     }
@@ -33,7 +64,15 @@ const IntroLogin = () => {
         <Logo size="large" />
         <Typography.Title level={4}>ระบบจัดการการสอบ</Typography.Title>
         {nextURL && <Alert message="กรุณาเข้าสู่ระบบเพื่อดำเนินการต่อ" type="info" showIcon />}
-        <GoogleLoginButton onClick={() => login('google')} />
+        <Spin
+          spinning={auth.loggingIn}
+          tip={auth.emailLoggingIn ? `กำลังเข้าสู่ระบบด้วยอีเมล ${auth.emailLoggingIn}...` : 'กำลังเข้าสู่ระบบ...'}
+        >
+          <GoogleLoginButton onClick={() => login('google')} />
+          <Divider>หรือ</Divider>
+          <EmailLinkLoginButton onClick={() => login('email')} />
+          <Typography.Text type="secondary">สำหรับกรรมการคุมสอบที่ไม่ได้ใช้บัญชี Google</Typography.Text>
+        </Spin>
       </Space>
     </Card>
   )
