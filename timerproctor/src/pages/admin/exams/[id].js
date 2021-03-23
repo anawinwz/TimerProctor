c17286ce4b0e8f5161bd5a24c51363e41c336b64
Anawin W <anawinwz@users.noreply.github.com>
2021-03-23 07:16:36 +0700

    Update timer on examStatus event

--- timerproctor/src/pages/admin/exams/[id].js
+++ timerproctor/src/pages/admin/exams/[id].js
@@ -47,7 +47,10 @@ const AdminExamPage = ({ match }) => {
           .on('connect_error', error => {
             throw error
           })
-          .on('examStatus', payload => exam.updateStatus(payload))
+          .on('examStatus', payload => {
+            exam.updateStatus(payload)
+            examAdmin.startTimer()
+          })
           .on('examAllowLogin', payload => examAdmin.updateAllowLogin(payload))
           .on('newTester', tester => examAdmin.addLocalTester(tester))
           .on('testerUpdate', payload => {
